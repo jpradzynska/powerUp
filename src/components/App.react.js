386d098a174506a833e1b70d536781ef653c7666
Joanna <asia@Joannas-MBP.lan>
2016-10-27 11:49:44 +0200

    Add nav links.

--- src/components/App.react.js
+++ src/components/App.react.js
@@ -1,23 +1,30 @@
-import React from 'react';
-import './App.scss';
-
-const yeomanImage = require('../images/yeoman.png');
+import React, { PropTypes } from 'react';
+import NavLink from './shared/NavLink.react';
+// import './App.scss';
 
 class AppComponent extends React.Component {
 
   render() {
     return (
       <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">
-          Please edit <code>src/components/App.js</code> to get started!
-        </div>
+        <h1>React Router Tutorial</h1>
+        <ul role="nav">
+          <li><NavLink to="/" onlyActiveOnIndex={true}>Home</NavLink></li>
+          <li><NavLink to="/about">About</NavLink></li>
+          <li><NavLink to="/releases">Releases</NavLink></li>
+          <li><NavLink to="/contact">Contact</NavLink></li>
+        </ul>
+        {this.props.children}
       </div>
     );
   }
 }
 
-AppComponent.defaultProps = {
+// AppComponent.defaultProps = {
+// };
+
+AppComponent.propTypes = {
+  actions: PropTypes.object.isRequired
 };
 
 export default AppComponent;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 import { Provider } from 'react-redux';
 import configureStore from './stores';
-import App from './containers/App';
+// import App from './containers/App';
+import App from './components/App.react';
+import About from './components/about/About.react';
+import Home from './components/home/Home.react';
+import Releases from './components/releases/Releases.react';
+import Contact from './components/contact/Contact.react';
+
 
 const store = configureStore();
 
-render(
+render((
   <Provider store={store}>
-    <App />
-  </Provider>,
-
-  document.getElementById('app')
-);
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/releases" component={Releases}/>
+        <Route path="/contact" component={Contact}/>
+      </Route>
+    </Router>
+  </Provider>
+), document.getElementById('app'));
