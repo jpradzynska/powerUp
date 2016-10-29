298fc3f7b9e5da73de2b9d16273e23543a754285
Joanna <asia@Joannas-MacBook-Pro.local>
2016-10-29 13:44:09 +0200

    Reorganize files.

--- src/components/App.react.js
+++ src/components/App.react.js
@@ -1,16 +1,18 @@
 import React, { PropTypes } from 'react';
 import NavLink from './shared/NavLink.react';
 import './App.scss';
+import diamondImg from '../images/power-up-diamond.svg';
 
 class AppComponent extends React.Component {
 
   render() {
     return (
-      <div className="index">
+      <div className="mainMenu">
         <h1>Power Up Web</h1>
         <ul>
           <li><NavLink to="/" onlyActiveOnIndex>Home</NavLink></li>
           <li><NavLink to="/about">About</NavLink></li>
+          <li><img src={diamondImg} alt="power-up diamond"/></li>
           <li><NavLink to="/releases">Releases</NavLink></li>
           <li><NavLink to="/contact">Contact</NavLink></li>
         </ul>
