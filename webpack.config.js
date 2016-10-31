9cb3e8a62450ff30db7a7545a1e5bec730ba8427
Joanna <asia@Joannas-MBP.lan>
2016-10-31 08:37:50 +0100

    Change webpack.config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,33 @@
 'use strict';
 
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const autoprefixer = require('autoprefixer');
-const postcss = require('postcss-scss');
+/* eslint no-console: "off" */
+const webpackConfigs = require('./conf/webpack');
+const defaultConfig = 'dev';
 
-module.exports = {
-  devtool: 'eval',
-  devServer: { hot: true },
-  entry: './src/index.js',
-  output: {
-    path: __dirname + '/dist',
-    filename: 'js/[name].js'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.css$/,
-        loader: ['style', 'css']
-      },
-      {
-        test: /\.scss$/,
-        loader: ['style', 'css', 'sass']
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel',
-        query: {
-          presets: ['react', 'es2015']
-        }
-      },
-      {
-        test: /\.(png|woff|woff2|eot|ttf|svg|swf)$/,
-        loader: 'file-loader?name=assets/[hash].[ext]'
-      }
-    ]
-  },
-  postcss: [
-    autoprefixer({ browsers: ['last 2 versions'] })
-  ],
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      chunks: ['main'],
-      favicon: './src/favicon.ico'
-    }),
-    new webpack.HotModuleReplacementPlugin()
-  ]
-}
+module.exports = (configName) => {
+
+  // If there was no configuration give, assume default
+  const requestedConfig = configName || defaultConfig;
+
+  // Return a new instance of the webpack config
+  // or the default one if it cannot be found.
+  let LoadedConfig = defaultConfig;
+
+  if (webpackConfigs[requestedConfig] !== undefined) {
+    LoadedConfig = webpackConfigs[requestedConfig];
+  } else {
+    console.warn(`
+      Provided environment "${configName}" was not found.
+      Please use one of the following ones:
+      ${Object.keys(webpackConfigs).join(' ')}
+    `);
+    LoadedConfig = webpackConfigs[defaultConfig];
+  }
+
+  const loadedInstance = new LoadedConfig();
+
+  // Set the global environment
+  process.env.NODE_ENV = loadedInstance.env;
+
+  return loadedInstance.config;
+};
