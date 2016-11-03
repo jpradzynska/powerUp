3c26348edd51e741d52179974777f6e2397ffc06
Joanna <asia@Joannas-MBP.lan>
2016-11-03 14:23:34 +0100

    Style contact section.

--- src/components/contact/Contact.react.js
+++ src/components/contact/Contact.react.js
@@ -9,23 +9,38 @@ class Contact extends React.Component {
   render() {
     return (
       <div className="component contact-component">
-        <div>
+        <div className="section-logo">
           <img alt="Power Up Logo" src="../../images/power-up-logo.svg" />
         </div>
-        <p>Contact</p>
-        <div>
-          <img alt="triangles" src="../../images/triangles-left.svg" />
-          <p>INFO</p>
-          <img alt="triangles" src="../../images/triangles-right.svg" />
+
+        <div className="section-contact">
+          <p>Contact</p>
         </div>
-        <div>
-          <p>Address:</p>
-          <p>ul. Bora Komorowskiego 19B</p>
-          <p>80-462 Gdańsk</p>
-          <p>Poland</p>
-          <p>Press / Business contact</p>
-          <p>contact@powerupgamestudio.com</p>
-          <img alt="e-mail button" src="../../images/contact/cta-email.svg" />
+
+        <div className="section-info">
+          <img alt="triangles" className="img-left" src="../../images/triangles-left.svg" />
+          <span>INFO</span>
+          <img alt="triangles" className="img-right" src="../../images/triangles-left.svg" />
+        </div>
+
+        <div className="contact-address">
+          <div className="address">
+            <p><span>Address:</span></p>
+            <p>ul. Bora Komorowskiego 19B</p>
+            <p>80-462 Gdańsk</p>
+            <p>Poland</p>
+          </div>
+          <div className="press">
+            <p><span>Press / Business contact</span></p>
+            <p>contact@powerupgamestudio.com</p>
+            <br />
+            <br />
+          </div>
+          <div className="email-btn">
+            <a href="mailto:contact@powerupgamestudio.com" title="Contact us">
+              <img alt="e-mail button" src="../../images/contact/cta-email.svg" />
+            </a>
+          </div>
         </div>
       </div>
     );
