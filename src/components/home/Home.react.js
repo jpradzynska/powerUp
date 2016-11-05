1191fe2dd294737ba9404c4cded511955c732189
Joanna <asia@Joannas-MacBook-Pro.local>
2016-11-05 13:36:38 +0100

    Change home style.

--- src/components/home/Home.react.js
+++ src/components/home/Home.react.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { layoutGenerator } from 'react-break';
 import ButtonComponent from '../shared/ButtonComponent.react';
 import HeaderComponent from '../shared/HeaderComponent.react';
 import LogoComponent from '../shared/LogoComponent.react';
 import './Home.scss';
 // import cssmodules from 'react-css-modules';
 // import styles from './Home.scss';
+
+const layout = layoutGenerator({
+  mobile: 0,
+  desktop: 960
+});
+
+const OnMobile = layout.is('mobile');
+const OnDesktop = layout.is('desktop');
 
 // @cssmodules(styles)
 class Home extends React.Component {
@@ -14,17 +23,27 @@ class Home extends React.Component {
     const BUTTON_IMG = '../../images/home/cta-projects.svg';
     const HEADER_TEXT_1 = 'Pixel based lifeforms';
     const HEADER_TEXT_2 = 'Presented by';
+
     return (
       <div className="component home-component">
-        <div className="mojdiv1">xxx</div>
-        <HeaderComponent headerText={HEADER_TEXT_1} />
-        <img alt="arrow" className="arrowDown" src="../../images/home/scroll.svg" />
-        <div className="mojdiv2">xxx</div>
-        <HeaderComponent headerText={HEADER_TEXT_2} />
-        <div className="mojdiv3">xxx</div>
-        <LogoComponent />
-        <ButtonComponent btnAddress={BUTTON_HREF} btnImg={BUTTON_IMG}/>
-        <div className="mojdiv4">xxx</div>
+        <OnMobile>
+          <LogoComponent />
+          <HeaderComponent headerText={HEADER_TEXT_1} />
+          <ButtonComponent btnAddress={BUTTON_HREF} btnImg={BUTTON_IMG}/>
+        </OnMobile>
+
+        <OnDesktop>
+          <div className="mojdiv1">xxx</div>
+          <HeaderComponent headerText={HEADER_TEXT_1} />
+          <img alt="arrow" className="arrowDown" src="../../images/home/scroll.svg" />
+          <div className="mojdiv2">xxx</div>
+          <HeaderComponent headerText={HEADER_TEXT_2} />
+          <img alt="arrow" className="arrowDown" src="../../images/home/scroll.svg" />
+          <div className="mojdiv3">xxx</div>
+          <LogoComponent />
+          <ButtonComponent btnAddress={BUTTON_HREF} btnImg={BUTTON_IMG}/>
+          <div className="mojdiv4">xxx</div>
+        </OnDesktop>
       </div>
     );
   }
